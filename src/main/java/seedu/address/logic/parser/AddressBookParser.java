2447885187ef1e0b856d2761ba4fd8706358dfa4
kayabuttertoastt <e0970063@u.nus.edu>
2023-10-19 02:18:48 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -49,47 +49,48 @@ public class AddressBookParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
 
-        // Note to developers: Change the log level in config.json to enable lower-level (i.e., FINE, FINER, and lower) log messages such as the one below.
+        // Note to developers: Change the log level in config.json to enable lower-level
+        // (i.e., FINE, FINER, and lower) log messages such as the one below.
         // Lower-level log messages are used sparingly to minimize noise in the code.
         logger.fine("Command word: " + commandWord + "; Arguments: " + arguments);
 
         switch (commandWord) {
-            case AddCommand.COMMAND_WORD:
-                return new AddCommandParser().parse(arguments);
+        case AddCommand.COMMAND_WORD:
+            return new AddCommandParser().parse(arguments);
 
-            case EditCommand.COMMAND_WORD:
-                return new EditCommandParser().parse(arguments);
+        case EditCommand.COMMAND_WORD:
+            return new EditCommandParser().parse(arguments);
 
-            case DeleteCommand.COMMAND_WORD:
-                return new DeleteCommandParser().parse(arguments);
+        case DeleteCommand.COMMAND_WORD:
+            return new DeleteCommandParser().parse(arguments);
 
-            case ClearCommand.COMMAND_WORD:
-                return new ClearCommand();
+        case ClearCommand.COMMAND_WORD:
+            return new ClearCommand();
 
-            case FindCommand.COMMAND_WORD:
-                return new FindCommandParser().parse(arguments);
+        case FindCommand.COMMAND_WORD:
+            return new FindCommandParser().parse(arguments);
 
-            case ListCommand.COMMAND_WORD:
-                return new ListCommand();
+        case ListCommand.COMMAND_WORD:
+            return new ListCommand();
 
-            case FindCourseCommand.COMMAND_WORD:
-                return new FindCourseCommandParser().parse(arguments);
+        case FindCourseCommand.COMMAND_WORD:
+            return new FindCourseCommandParser().parse(arguments);
 
-            case FindRoleCommand.COMMAND_WORD:
-                return new FindRoleCommandParser().parse(arguments);
+        case FindRoleCommand.COMMAND_WORD:
+            return new FindRoleCommandParser().parse(arguments);
 
-            case FindTutorialCommand.COMMAND_WORD:
-                return new FindTutorialCommandParser().parse(arguments);
+        case FindTutorialCommand.COMMAND_WORD:
+            return new FindTutorialCommandParser().parse(arguments);
 
-            case ExitCommand.COMMAND_WORD:
-                return new ExitCommand();
+        case ExitCommand.COMMAND_WORD:
+            return new ExitCommand();
 
-            case HelpCommand.COMMAND_WORD:
-                return new HelpCommand();
+        case HelpCommand.COMMAND_WORD:
+            return new HelpCommand();
 
             default:
-                logger.finer("This user input caused a ParseException: " + userInput);
-                throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+            logger.finer("This user input caused a ParseException: " + userInput);
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 }
