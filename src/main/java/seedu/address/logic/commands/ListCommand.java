70e654748d6cd6da27cd502403bdae82359f9b79
wjacobw <williamjacob0910@gmail.com>
2023-10-19 16:16:57 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,20 +1,21 @@
 package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import java.util.ArrayList;
+import java.util.Set;
 import seedu.address.model.person.Role;
 import seedu.address.model.person.Contact;
 import seedu.address.model.person.Tutorial;
 import seedu.address.model.person.Course;
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
-import java.util.ArrayList;
-import java.util.Set;
+
 
 /**
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
-    AddressBook ab = new AddressBook();
+
     public static final String COMMAND_WORD = "list";
     public static final String enter = "\n      ";
     public static int numPeople = 0;
@@ -25,7 +26,7 @@ public class ListCommand extends Command {
     ArrayList<Set<Course>> courseList = new ArrayList<>();
     ArrayList<Set<Tutorial>> tutorialList = new ArrayList<>();
 
-    String roleString(Set<Role> roleList) {
+    String roleToString(Set<Role> roleList) {
         String begin = "Role: ";
         StringBuilder lst = new StringBuilder(begin);
         roleList.forEach(role -> lst.append(role.toString()).append(", "));
@@ -35,7 +36,7 @@ public class ListCommand extends Command {
         return lst.toString();
     }
 
-    String contactString(Set<Contact> contactList) {
+    String contactToString(Set<Contact> contactList) {
         String begin = "Contact: ";
         StringBuilder lst = new StringBuilder(begin);
         contactList.forEach(contact -> lst.append(contact.toString()).append(", "));
@@ -45,7 +46,7 @@ public class ListCommand extends Command {
         return lst.toString();
     }
 
-    String courseString(Set<Course> courseList) {
+    String courseToString(Set<Course> courseList) {
         String begin = "Courses: ";
         StringBuilder lst = new StringBuilder(begin);
         courseList.forEach(course -> lst.append(course.toString()).append(", "));
@@ -55,7 +56,7 @@ public class ListCommand extends Command {
         return lst.toString();
     }
 
-    String tutorialString(Set<Tutorial> tutorialList) {
+    String tutorialToString(Set<Tutorial> tutorialList) {
         String begin = "Tutorials: ";
         StringBuilder lst = new StringBuilder(begin);
         courseList.forEach(tutorial-> lst.append(tutorial.toString()).append(", "));
@@ -85,8 +86,8 @@ public class ListCommand extends Command {
 
         for (int i = 0; i < nameList.size(); i++) {
             peopleList += (i + 1) +".  Name: " + nameList.get(i) + enter
-                    + roleString(roleList.get(i)) + enter  + contactString(contactList.get(i)) + enter
-                    + courseString(courseList.get(i)) + enter  + tutorialString(tutorialList.get(i)) + "\n";
+                    + roleToString(roleList.get(i)) + enter  + contactToString(contactList.get(i)) + enter
+                    + courseToString(courseList.get(i)) + enter  + tutorialToString(tutorialList.get(i)) + "\n";
         }
 
         requireNonNull(model);
