9b00e357f0679748d3dba77f9ab862abf175de65
fry <E0968906@u.nus.edu>
2023-10-19 22:20:01 +0800

    Update test files in testutil

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -1,96 +1,97 @@
-//package seedu.address.testutil;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import seedu.address.model.person.Address;
-//import seedu.address.model.person.Email;
-//import seedu.address.model.person.Name;
-//import seedu.address.model.person.Person;
-//import seedu.address.model.person.Phone;
-//import seedu.address.model.tag.Tag;
-//import seedu.address.model.util.SampleDataUtil;
-//
-///**
-// * A utility class to help with building Person objects.
-// */
-//public class PersonBuilder {
-//
-//    public static final String DEFAULT_NAME = "Amy Bee";
-//    public static final String DEFAULT_PHONE = "85355255";
-//    public static final String DEFAULT_EMAIL = "amy@gmail.com";
-//    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-//
-//    private Name name;
-//    private Phone phone;
-//    private Email email;
-//    private Address address;
-//    private Set<Tag> tags;
-//
-//    /**
-//     * Creates a {@code PersonBuilder} with the default details.
-//     */
-//    public PersonBuilder() {
-//        name = new Name(DEFAULT_NAME);
-//        phone = new Phone(DEFAULT_PHONE);
-//        email = new Email(DEFAULT_EMAIL);
-//        address = new Address(DEFAULT_ADDRESS);
-//        tags = new HashSet<>();
-//    }
-//
-//    /**
-//     * Initializes the PersonBuilder with the data of {@code personToCopy}.
-//     */
-//    public PersonBuilder(Person personToCopy) {
-//        name = personToCopy.getName();
-//        phone = personToCopy.getPhone();
-//        email = personToCopy.getEmail();
-//        address = personToCopy.getAddress();
-//        tags = new HashSet<>(personToCopy.getTags());
-//    }
-//
-//    /**
-//     * Sets the {@code Name} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withName(String name) {
-//        this.name = new Name(name);
-//        return this;
-//    }
-//
-//    /**
-//     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withTags(String ... tags) {
-//        this.tags = SampleDataUtil.getTagSet(tags);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Address} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withAddress(String address) {
-//        this.address = new Address(address);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Phone} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withPhone(String phone) {
-//        this.phone = new Phone(phone);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Email} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withEmail(String email) {
-//        this.email = new Email(email);
-//        return this;
-//    }
-//
-//    public Person build() {
-//        return new Person(name, phone, email, address, tags);
-//    }
-//
-//}
+package seedu.address.testutil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.person.Contact;
+import seedu.address.model.person.Course;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Role;
+import seedu.address.model.person.Tutorial;
+import seedu.address.model.util.SampleDataUtil;
+
+/**
+ * A utility class to help with building Person objects.
+ */
+public class PersonBuilder {
+
+    public static final String DEFAULT_NAME = "Amy Bee";
+    public static final String DEFAULT_ROLE = "Student";
+    public static final String DEFAULT_CONTACT = "amy@gmail.com";
+    public static final Course DEFAULT_COURSE = new Course("CS2103T");
+    public static final Tutorial DEFAULT_TUTORIAL = new Tutorial(DEFAULT_COURSE, "F08");
+
+    private Name name;
+    private Set<Role> roles;
+    private Set<Contact> contacts;
+    private Set<Course> courses;
+    private Set<Tutorial> tutorials;
+
+    /**
+     * Creates a {@code PersonBuilder} with the default details.
+     */
+    public PersonBuilder() {
+        name = new Name(DEFAULT_NAME);
+        roles = new HashSet<>();
+        contacts = new HashSet<>();
+        courses = new HashSet<>();
+        tutorials = new HashSet<>();
+    }
+
+    /**
+     * Initializes the PersonBuilder with the data of {@code personToCopy}.
+     */
+    public PersonBuilder(Person personToCopy) {
+        name = personToCopy.getName();
+        roles = new HashSet<>(personToCopy.getRoles());
+        contacts = new HashSet<>(personToCopy.getContacts());
+        courses = new HashSet<>(personToCopy.getCourses());
+        tutorials = new HashSet<>(personToCopy.getTutorials());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code roles} into a {@code Set<Role>} and set it to the {@code Person} that we are building.
+     */
+    public PersonBuilder withRoles(String... roles) {
+        this.roles = SampleDataUtil.getRolesSet(roles);
+        return this;
+    }
+
+    /**
+     * Parses the {@code contacts} into a {@code Set<Contact>} and set it to the {@code Person} that we are building.
+     */
+    public PersonBuilder withContacts(String... contacts) {
+        this.contacts = SampleDataUtil.getContactSet(contacts);
+        return this;
+    }
+
+    /**
+     * Parses the {@code courses} into a {@code Set<Course>} and set it to the {@code Person} that we are building.
+     */
+    public PersonBuilder withCourses(Course... courses) {
+        this.courses = SampleDataUtil.getCourseSet(courses);
+        return this;
+    }
+
+    /**
+     * Parses the {@code tutorials} into a {@code Set<Tutorial>} and set it to the {@code Person} that we are building.
+     */
+    public PersonBuilder withTutorials(Tutorial... tutorials) {
+        this.tutorials = SampleDataUtil.getTutorialSet(tutorials);
+        return this;
+    }
+
+    public Person build() {
+        return new Person(name, roles, contacts, courses, tutorials);
+    }
+
+}
