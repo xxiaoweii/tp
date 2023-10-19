172bfc34cfeebd0a5a33b510a69cb5d305e62ca0
xxiaoweii <e0967899@u.nus.edu>
2023-10-20 07:15:40 +0800

    Update Role.java

--- src/test/java/seedu/address/testutil/Assert.java
+++ src/test/java/seedu/address/testutil/Assert.java
@@ -1,20 +1,20 @@
-// package seedu.address.testutil;
+//package seedu.address.testutil;
 //
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.function.Executable;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.function.Executable;
 //
-// /**
-// * A set of assertion methods useful for writing tests.
+///**
+//* A set of assertion methods useful for writing tests.
+//*/
+//public class Assert {
+//
+///**
+// * Asserts that the {@code executable} throws the {@code expectedType} Exception.
+// * This is a wrapper method that invokes {@link Assertions#assertThrows(Class, Executable)},
+// * to maintain consistency with our custom {@link #assertThrows(Class, String, Executable)} method.
+// * To standardize API calls in this project, users should use this method instead of
+// * {@link Assertions#assertThrows(Class, Executable)}.
 // */
-// public class Assert {
-//
-//    /**
-//     * Asserts that the {@code executable} throws the {@code expectedType} Exception.
-//     * This is a wrapper method that invokes {@link Assertions#assertThrows(Class, Executable)},
-//     * to maintain consistency with our custom {@link #assertThrows(Class, String, Executable)} method.
-//     * To standardize API calls in this project, users should use this method instead of
-//     * {@link Assertions#assertThrows(Class, Executable)}.
-//     */
 //    public static void assertThrows(Class<? extends Throwable> expectedType, Executable executable) {
 //        Assertions.assertThrows(expectedType, executable);
 //    }
@@ -31,4 +31,4 @@
 //        Throwable thrownException = Assertions.assertThrows(expectedType, executable);
 //        Assertions.assertEquals(expectedMessage, thrownException.getMessage());
 //    }
-// }
+//}
