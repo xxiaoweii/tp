3ae30f94f2a3c7d6837f9a9f89da5966e2eb4fb8
fry <E0968906@u.nus.edu>
2023-10-29 20:20:30 +0800

    Fix list bug

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -62,7 +62,6 @@ public class ListCommand extends Command {
     private String tutorialToString(Set<Tutorial> tutorialList) {
         String begin = "Tutorials: ";
         StringBuilder lst = new StringBuilder(begin);
-        courseList.forEach(tutorial-> lst.append(tutorial.toString()).append(", "));
         tutorialList.forEach(tutorial-> lst.append(tutorial.toString()).append(", "));
         if (lst.length() > begin.length()) {
             lst.setLength(lst.length() - 2);
