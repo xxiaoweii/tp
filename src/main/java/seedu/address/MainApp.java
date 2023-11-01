e26d7df346c82f0de2d9ce9337fbe294c7ad7cd5
xxiaoweii <e0967899@u.nus.edu>
2023-11-01 17:00:52 +0800

    Update version number for next JAR release

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -36,7 +36,7 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 3, 0, true);
+    public static final Version VERSION = new Version(1, 3, 1, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
@@ -170,7 +170,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting NUSearch " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
