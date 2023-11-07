6610b7c6152d13e082b3153aa0bcb28b0ca8035d
kayabuttertoastt <e0970063@u.nus.edu>
2023-11-08 02:29:26 +0800

    Add javadocs

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -36,18 +36,39 @@ public class CommandResult {
         this(feedbackToUser, false, false);
     }
 
+    /**
+     * Retrieves the feedback message to be displayed to the user.
+     *
+     * @return The feedback message.
+     */
     public String getFeedbackToUser() {
         return feedbackToUser;
     }
 
+    /**
+     * Checks if help information should be displayed to the user.
+     *
+     * @return True if help information should be displayed, otherwise returns false.
+     */
     public boolean isShowHelp() {
         return showHelp;
     }
 
+    /**
+     * Checks if the application should exit.
+     *
+     * @return True if the application should exit, otherwise returns false.
+     */
     public boolean isExit() {
         return exit;
     }
 
+    /**
+     * Checks equality of the CommandResult with another object.
+     *
+     * @param other The reference object with which to compare for equality.
+     * @return True if the objects are equal, otherwise returns false.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -65,11 +86,21 @@ public class CommandResult {
                 && exit == otherCommandResult.exit;
     }
 
+    /**
+     * Returns a hash code value for the CommandResult object.
+     *
+     * @return A hash code value for this CommandResult.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(feedbackToUser, showHelp, exit);
     }
 
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return A string representation of this CommandResult.
+     */
     @Override
     public String toString() {
         return new ToStringBuilder(this)
