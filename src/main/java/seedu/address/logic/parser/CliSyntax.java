064a3b59892659332054808bf1848266dfda1e21
xxiaoweii <e0967899@u.nus.edu>
2023-11-10 22:21:54 +0800

    Edit the valid prefix for adding a person

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,10 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("--name");
-    public static final Prefix PREFIX_ROLE = new Prefix("--role");
-    public static final Prefix PREFIX_CONTACT = new Prefix("--contact");
-    public static final Prefix PREFIX_COURSE = new Prefix("--course");
+    public static final Prefix PREFIX_NAME = new Prefix("--name ");
+    public static final Prefix PREFIX_ROLE = new Prefix("--role ");
+    public static final Prefix PREFIX_CONTACT = new Prefix("--contact ");
+    public static final Prefix PREFIX_COURSE = new Prefix("--course ");
     public static final Prefix PREFIX_TUTORIAL = new Prefix("--tutorial");
     public static final Prefix PREFIX_ADD = new Prefix("--add");
     public static final Prefix PREFIX_DELETE = new Prefix("--delete");
